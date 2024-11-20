ae3bd27057d273eaab15feccd42b3abbda8cfe6d
Wouter Mevius <wouter.mevius@digitalagency.nl>
2024-11-20 14:16:32 +0100

    bette implemenattion

--- topic-receiver.js
+++ topic-receiver.js
@@ -1,65 +1,117 @@
-
 var azure = require('@azure/service-bus');
 
-module.exports = async function (RED) {
+module.exports = function (RED) {
 
-    let client;
-    let receiver;
+    var client;
+    var receiver;
+    var running = false;
 
-    function ReceiveMessage(config) {
+    var statusEnum = {
+        disconnected: { color: "red", text: "Disconnected" },
+        connected: { color: "green", text: "Connected" },
+        sent: { color: "yellow", text: "Sent message" },
+        received: { color: "blue", text: "Message received" },
+        error: { color: "grey", text: "Error" }
+    };
 
-        RED.nodes.createNode(this, config);
+    var setStatus = function (node, status) {
+        node.status({ fill: status.color, shape: "dot", text: status.text });  
+    }
 
-        var node = this;
+    var myMessageHandler = async (node, message) => {
+        if (node.connectivityFeedbackTimeout) {
+            clearTimeout(node.connectivityFeedbackTimeout);
+        }
+        node.connectivityFeedbackTimeout = setTimeout(function () { 
+            setStatus(node, statusEnum.connected);
+        }, 2000);
 
-        node.log("Creating receive-message-topic node ");
+        setStatus(node, statusEnum.received);
 
-        client = new azure.ServiceBusClient(config.connectionString);
+        console.log("Message received: ", message.body);
+
+        node.send({ payload: message.body });
+    };
+
+
+    var myErrorHandler = async (node, args) => {
+        node.error(args.error);
+        setStatus(node, statusEnum.error);
+    };
+
+    var connectToServiceBus = (node, config) => {
+
+        node.log("Connecting to Azure Service Bus Topic: " + config.topic);
+
+        let client = new azure.ServiceBusClient(config.connectionString);
 
         let receiverOptions = {
             receiveMode: 'receiveAndDelete'  // Receive and delete the message   
         }
 
-        let connectivityFeedbackTimeout;
 
-        const myMessageHandler = async (message) => { 
-            if (connectivityFeedbackTimeout) {
-                clearTimeout(connectivityFeedbackTimeout);
-            }
-
-            node.status({ fill: "blue", shape: "ring", text: "message received" });
-            connectivityFeedbackTimeout = setTimeout(() => { node.status({ fill: "green", shape: "dot", text: "connected" }); }, 2000);
-            console.log("Message received: ", message.body);
-            node.send({ payload:  message.body });   
-
-        };
-        const myErrorHandler = async (args) => {
-            node.error(args.error);
-            node.status({ fill: "red", shape: "ring", text: "error, see debug or output" });
-        };
-
-
-        receiver = client.createReceiver(config.topic, config.subscription, receiverOptions);
+        let receiver = client.createReceiver(config.topic, config.subscription, receiverOptions);
 
         let subscribeOptions = {
             autoComplete: false,
             maxConcurrentCalls: 1
         };
 
-        receiver.subscribe({
-            processMessage: myMessageHandler,
-            processError: myErrorHandler
+        node.close = receiver.subscribe({
+            processMessage: async (msg) => await myMessageHandler(node, msg),
+            processError:  async (args) => await myErrorHandler(node, args)
         }, subscribeOptions);
 
-        node.log("Subribed and listening for messages");
+        node.client = client;   
+        node.receiver = receiver;   
 
-        node.on("close", async function (done) {
-            node.log("Closing topic receiver - " + node.id);
-            await receiver.close();
-            await client.close();
-            done();
-        });
+        setStatus(node, statusEnum.connected);
+
     }
+
+    var disconnectFromServiceBus = function (node) {
+        if (node.reconnectTimer) {
+            clearTimeout(node.reconnectTimer);
+            node.reconnectTimer = null;
+        }
+
+        if (node.close) {
+            node.close();
+            node.close = null;
+        }
+
+        if (node.client) {
+            node.log('Disconnecting from Azure Service Bus');
+            node.client.close();
+            node.client = null;
+            node.receiver.close();
+            node.receiver = null;
+            setStatus(node, statusEnum.disconnected);
+        }
+    };
+
+
+    // Main function called by Node-RED    
+    function ReceiveMessage(config) {
+
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+
+        running = true; // flag to indicate the node is running 
+
+        node.config = config; // save the config so we can access it later
+
+        connectToServiceBus(this, config);
+
+        node.on("close", function () {
+            disconnectFromServiceBus(node, this);
+        });
+
+    }
+
+
+
 
     RED.nodes.registerType("receive-azure-sb-topic", ReceiveMessage);
 }
