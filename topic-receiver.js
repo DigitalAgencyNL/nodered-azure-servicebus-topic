51d1df39c26f4b4424ddeb0e2a4a9843212bde2b
Wouter Mevius <wouter.mevius@digitalagency.nl>
2024-11-20 10:48:08 +0100

    topic receiver

--- topic-receiver.js
+++ topic-receiver.js
@@ -1,10 +1,7 @@
 
-var azure = require('@azure/service-bus');  
-
-const nodeRedNodeTestHelper = require("node-red-node-test-helper");
+var azure = require('@azure/service-bus');
 
 module.exports = async function (RED) {
-
 
     let client;
     let receiver;
@@ -17,34 +14,32 @@ module.exports = async function (RED) {
 
         node.log("Creating receive-message-topic node ");
 
-
         client = new azure.ServiceBusClient(config.connectionString);
 
         let receiverOptions = {
             receiveMode: 'receiveAndDelete'  // Receive and delete the message   
         }
 
+        let connectivityFeedbackTimeout;
 
-        const myMessageHandler = async (message) => {
-        
-           
-            node.status({ fill: "blue", shape: "ring", text: "received a message" });
+        const myMessageHandler = async (message) => { 
+            if (connectivityFeedbackTimeout) {
+                clearTimeout(connectivityFeedbackTimeout);
+            }
 
-            node.send(message);    
+            node.status({ fill: "blue", shape: "ring", text: "message received" });
+            connectivityFeedbackTimeout = setTimeout(() => { node.status({ fill: "green", shape: "dot", text: "connected" }); }, 2000);
+            console.log("Message received: ", message.body);
+            node.send({ payload:  message.body });   
 
-            setTimeout(() => { node.status({ fill: "green", shape: "dot", text: "connected" }); }, 2000);
         };
-        const myErrorHandler = async (args) => { 
+        const myErrorHandler = async (args) => {
             node.error(args.error);
-            node.status({ fill: "red", shape: "ring", text: "error, see debug or output" }); 
-
+            node.status({ fill: "red", shape: "ring", text: "error, see debug or output" });
         };
 
 
-
-        receiver = client.createReceiver(config.topicName, config.subscriptionName, receiverOptions);
-
-
+        receiver = client.createReceiver(config.topic, config.subscription, receiverOptions);
 
         let subscribeOptions = {
             autoComplete: false,
@@ -59,14 +54,12 @@ module.exports = async function (RED) {
         node.log("Subribed and listening for messages");
 
         node.on("close", async function (done) {
-
-            node.log("Closing " + d + " - " + node.id);
+            node.log("Closing topic receiver - " + node.id);
             await receiver.close();
-
-            await client.close(); 
+            await client.close();
             done();
-        }); 
+        });
     }
- 
+
     RED.nodes.registerType("receive-azure-sb-topic", ReceiveMessage);
 }
